79e06d8ec719b6c41937aa0bb6438f5c3e534d62
masesm5@yahoo.com <maseskrikorian@gmail.com>
2020-09-23 22:31:42 -0700

    v0.1

---
+++ test/test-client/Client.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <masesk/EasySocket.hpp>
+#include <string>
+using namespace std;
+using namespace masesk;
+int main() {
+	EasySocket socketManager;
+	socketManager.socketConnect("test", "127.0.0.1", 8080);
+	string userInput;
+	while (true) {
+		cout << "> ";
+		getline(cin, userInput);
+		if (userInput.size() <= 0) {
+			break;
+		}
+		socketManager.socketSend("test", userInput);
+	}
+	socketManager.closeConnection("test");
+	return 0;
+}
